60046938524778b69763170e1880b252f866c435
chronorose <rendemare@gmail.com>
2023-09-11 05:59:15 +0300

    cicl 1

---
+++ cicl_zadanie_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    unsigned long x = 0;
+    scanf("%lu", &x);
+    for(unsigned long i = 0; i <= x; i++) {
+        int c = 0;
+        if(i % 3 == 0) {
+            printf("fizz");
+            c++;
+        } 
+        if(i % 5 == 0) {
+            printf("buzz");
+            c++;
+        }
+        if (c == 0) {
+            printf("%lu ", i);
+        } else {
+            printf(" ");
+        }
+    }
+}
