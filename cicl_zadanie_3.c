260afc9196bef6b994f965dcfcce7d884121319f
chronorose <rendemare@gmail.com>
2023-09-11 06:50:46 +0300

    treugolka

--- cicl_zadanie_3.c
+++ cicl_zadanie_3.c
@@ -5,14 +5,22 @@
 // 4 5 6
 // 7 8 9 10
 // 11 12 13
+// 5 * (6) / 2 - 1
 
 int main() {
     unsigned long x = 0;
     scanf("%lu", &x);
-    for(unsigned long i = 0; i < x; i++) {
-        for(unsigned long j = x * i + 1; j <= x * (i + 1); j++) {
-            printf("%lu ", j); 
+    int count = 0;
+    unsigned long sc = (x * (x + 1)) / 2 - 1;
+    for(unsigned long i = 0; i <= sc;) {
+        if(i + count > sc) {
+            count = sc - i;
+        }
+        for(int j = 0; j <= count; j++) {
+            printf("%lu ", i + j);
         }
         printf("\n");
+        count++;
+        i += count;
     }
 }
