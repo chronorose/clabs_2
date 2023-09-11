a77bacafebe0205909b015e9e866f9184cac9823
chronorose <rendemare@gmail.com>
2023-09-11 06:18:36 +0300

    4

--- cicl_zadanie_3.c
+++ cicl_zadanie_3.c
@@ -1,5 +1,11 @@
 #include <stdio.h> 
 
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13
+
 int main() {
     unsigned long x = 0;
     scanf("%lu", &x);
---
+++ cicl_zadanie_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    unsigned long x = 0;
+    scanf("%lu", &x);
+    if(x < 2) {
+        printf("exception: menshe 2\n");
+    }
+    unsigned long i = x / 2 + 1;
+    while(i >= 2) {
+        if(x % i == 0) {
+            i = 0;
+        } else {
+            i--;
+        }
+    } 
+    if(i == 1) {
+        printf("prostoe\n");
+    } else {
+        printf("ne prostoe\n");
+    }
+}
