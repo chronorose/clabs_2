d89360f3ee617faecdd9cf692e9d55e6535e84b0
chronorose <rendemare@gmail.com>
2023-09-18 06:17:07 +0300

    funs1

---
+++ functions/functions_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int check(unsigned long x) {
+    size_t n = x / 2;
+    size_t i = 2;
+    short temp = 0;
+    while(i <= n) {
+        if(x % i == 0) {
+            i = 0;
+            temp++;
+            break;
+        }
+        i++;
+    }
+    if(temp) return 0;
+    return 1;
+}
+
+int main() {
+    unsigned long x = 0;
+    scanf("%lu", &x);
+    if(x < 2) {
+        printf("error; x < 2");
+        return 1;
+    }
+    for(size_t i = 2; i <= x; i++) {
+        if(check(i)) {
+            printf("%lu ", i);
+        }
+    }
+}
