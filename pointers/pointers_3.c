2c6ce8f0e1d6e7d32a3acd7e800110cf7b295f0f
chronorose <rendemare@gmail.com>
2023-09-24 12:52:02 +0300

    star?

--- pointers/pointers_3.c
+++ pointers/pointers_3.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
+#include <stddef.h>
 
+void infinite_recursion(char* start, char* current) {
+    char v;
+    current = &v;
+    ptrdiff_t result = start - current;
+    printf("%td\n", result / 1024);
+    infinite_recursion(start, current);
+}
 
+int main() {
+    char start;
+    infinite_recursion(&start, NULL);
+}
