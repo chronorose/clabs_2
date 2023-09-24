3c58d9e3d81c1a0328fae26874c40fe462764a3f
chronorose <rendemare@gmail.com>
2023-09-24 12:59:01 +0300

    comments etc

--- one-dim-static-array/main.c
+++ one-dim-static-array/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+// problem 1
 void scanArr(int* arr, int n) {
     for(size_t i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 }
 
+// problem 2
 void printArr(int* arr, int n) {
     for(size_t i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -22,12 +24,14 @@ void swap(int* a, int* b) {
     /* *a = *a ^ *b; */
 }
 
+// problem 3
 void revertArray(int* arr, int n) {
     for(size_t i = 0; i <= n / 2; i++) {
         swap(&arr[i], &arr[n - i - 1]);
     }
 }
 
+// problem 4
 int maxInArray(int *arr, int n) {
     int max = arr[0];
     for(size_t i = 1; i < n; i++) {
@@ -36,6 +40,7 @@ int maxInArray(int *arr, int n) {
     return max;
 }
 
+// problem 5
 size_t findInArr(int* arr, int n, int x) {
     size_t i = 0;
     while(arr[i] != x && i < n) i++; 
@@ -53,6 +58,7 @@ size_t numberOfDigits(int x) {
     return i;
 }
 
+// problem 6
 void extractDigits(int* arr, int x, int n) {
     size_t xSize = numberOfDigits(x);      
     if(xSize > n) xSize = xSize - (xSize - n); 
@@ -66,6 +72,7 @@ void extractDigits(int* arr, int x, int n) {
     }
 }
 
+// problem 7
 int compareArrays(int* arr1, int* arr2, int n1, int n2) {
     size_t iter = n1 < n2 ? n1 : n2;
     size_t i = 0;
@@ -78,6 +85,7 @@ int compareArrays(int* arr1, int* arr2, int n1, int n2) {
     return 0;
 }
 
+// tests for problem 7
 void compareArraysTest() {
     int arr1[32];
     int arr2[32];
@@ -96,18 +104,25 @@ void compareArraysTest() {
 
 int main() {
     int arr[32];    
-    int arr_for_digits[30];
     int n = 6;
+
     scanArr(arr, n);
+
     int c = findInArr(arr, n, 6);
+
     revertArray(arr, n);
     printf("[revertArray test]:\n");
     printArr(arr, n);
+
     printf("maxInArray -> %d\n", maxInArray(arr, n));
+
     printf("findInArray(6) -> %d\n", c);
-    printf("[extractDigits test, x = 340123]:\n");
+
+    int arr_for_digits[30];
     for(size_t i = 0; i < 30; i++) arr_for_digits[i] = i + 1;
     extractDigits(arr_for_digits, 340123, 30);
+    printf("[extractDigits test, x = 340123]:\n");
     printArr(arr_for_digits, 10);
+
     compareArraysTest();
 }
