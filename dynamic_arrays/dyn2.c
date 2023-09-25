df34a2ce084c6b53099630d496355a6ed4ca6cdb
chronorose <rendemare@gmail.com>
2023-09-25 12:49:26 +0300

    asdkasdjqwejk

--- dynamic_arrays/dyn2.c
+++ dynamic_arrays/dyn2.c
@@ -9,26 +9,44 @@ void nullCheck(void* ptr) {
 }
 
 void allocateArr(int** ptr, size_t n) {
-    *ptr = malloc(sizeof(int));
+    *ptr = malloc(sizeof(int) * n);
     nullCheck(*ptr);
 }
 
 int* initArray(int n) {
     int* arr;
     allocateArr(&arr, n);
-    for(int i = 0; i < n; i++) arr[i] = i;
+    for(size_t i = 0; i < n; i++) arr[i] = i;
     return arr;
 }
 
 void printArr(int* arr, int n) {
-    for(int i = 0; i < n; i++) {
+    for(size_t i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-int* findSubArr(int* arr1, int* arr2, )
+int* findSubArr(int* arr1, int* arr2, int n1, int n2) {
+    for(size_t i = 0; i < n1; i++) {
+        size_t j = 0;
+        int* mem = &arr1[i];
+        while(i + j < n1 && arr1[i + j] == arr2[j] && j < n2) {
+            if(j == n2 - 1 && arr1[i + j] == arr2[j]) return mem;
+            j++; 
+        }
+    }
+    return NULL;
+}
 
 int main() {
-    
+    int n1 = 10;
+    int n2 = 3;
+    int* arr1 = initArray(n1);
+    int* arr2 = malloc(sizeof(int) * n2); 
+    for(int i = 0; i < n2; i++) arr2[i] = i + 7;
+    int* ptr = findSubArr(arr1, arr2, n1, n2);
+    printArr(arr1, n1);
+    printArr(arr2, n2);
+    printf("%p\n", ptr);
 }
