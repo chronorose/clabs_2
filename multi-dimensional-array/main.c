ba598f90650f7b9fdf68498bf93072aa365fadc9
chronorose <rendemare@gmail.com>
2023-09-25 06:49:04 +0300

    qweqwdasd

---
+++ multi-dimensional-array/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void scanArr(int arr[4][4]) {
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+}
+
+void printArr(int arr[4][4]) {
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            printf("%2d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void swap(int* arr1, int* arr2) {
+    int temp = *arr1;
+    *arr1 = *arr2;
+    *arr2 = temp;
+}
+
+void reverseArr(int arr[4][4]) {
+    int count = 0;
+    int size = 4;
+    for(int i = 0; i < size; i++) {
+        for(int j = i + 1; j < size; j++) {
+            swap(&arr[i][j], &arr[j][i]);
+            /* swap(&arr[size - i - 1][size - j - 1], &arr[i][j]); */
+            /* arr[size - i - 1][size - j - 1] = arr[i][j]; */
+        }
+    }
+}
+
+int main() {
+    int arr[4][4];
+    scanArr(arr);
+    printArr(arr);
+    printf("\n");
+    reverseArr(arr);
+    printArr(arr);
+}
